62bcf42620d47e918a333d8b10f2b250f0a39ce8
ChuckMcDuck <sharp_shot@sbcglobal.net>
2014-12-02 13:27:45 -0600

    added constructors to penguin

--- Penguin.java
+++ Penguin.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
+import info.gridworld.actor.Bug;
 
-public class Penguin {
 
+public class Penguin extends Bug
+{
+	public Penguin()
+	{
+		super(null);
+	}
+	
+	public Penguin(Color initialColor)
+	{
+		super(initialColor);
+	}
+	
+	
 }
